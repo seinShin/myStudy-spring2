7067352d9d01b66e12f0fa9168e9be5afddfdb55
seinShin <tlstpdls13@gmail.com>
2023-11-22 21:16:15 +0900

    feat : 추상화에 의존하도록 변경 * AppConfig를 통해 구체 클래스를 선택함으로써 관심사를 분리하도록 코드 변경

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,13 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    // 생성자 주입, 객체를 밖에서 넣어줌.
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
